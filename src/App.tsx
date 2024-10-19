3a8b66071e30e6c406d93e3ef3d7701323b65432
RakibMojumder <sujonahmed45a4@gmail.com>
2024-10-19 12:38:01 +0600

    text change in banner section

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,29 @@ const App = () => {
       <div className="max-w-[1440px] mx-auto px-5 flex justify-center items-center">
         <div className="h-full">
           <Banner />
-          <div className="text-white grid grid-cols-4 gap-3 lg:gap-10 w-full lg:w-[750px] mx-auto">
+          <div className="text-white grid grid-cols-4 gap-1 lg:gap-10 w-full lg:w-[750px] mx-auto">
             <div className="flex justify-center items-center flex-col gap-y-3 text-center">
               <img src={imageOne} alt="icon" className="size-6 lg:size-12" />
-              <h5 className="text-xs lg:text-base">
+              <h5 className="text-[10px] lg:text-base">
                 Up to 30% more conversions
               </h5>
             </div>
             <div className="flex justify-center items-center flex-col gap-y-3 text-center">
               <img src={imageTwo} alt="icon" className="size-6 lg:size-12" />
-              <h5 className="text-xs lg:text-base">
-                Up to 30% more conversions
+              <h5 className="text-[10px] lg:text-base">
+                Competitive <br /> Advantage
               </h5>
             </div>
             <div className="flex justify-center items-center flex-col gap-y-3 text-center">
               <img src={imageThree} alt="icon" className="size-6 lg:size-12" />
-              <h5 className="text-xs lg:text-base">
-                Up to 30% more conversions
+              <h5 className="text-[10px] lg:text-base">
+                GDRP <br /> Compliant
               </h5>
             </div>
             <div className="flex justify-center items-center flex-col gap-y-3 text-center">
               <img src={imageFour} alt="icon" className="size-6 lg:size-12" />
-              <h5 className="text-xs lg:text-base">
-                Up to 30% more conversions
+              <h5 className="text-[10px] lg:text-base">
+                Faster <br /> website
               </h5>
             </div>
           </div>
